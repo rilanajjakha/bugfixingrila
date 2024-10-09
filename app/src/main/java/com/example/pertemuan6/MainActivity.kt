dcd57a5d0cb1fe18542fa50c846d336a452345de
Rila <rilanajjakha@mail.ugm.ac.id>
2024-10-09 11:07:22 +0700

    initial commit

--- app/src/main/java/com/example/pertemuan6/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan6/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.DatePicker.OnDateChangedListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pertemuan6.databinding.ActivityMainBinding
 import java.util.Calendar
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -30,27 +28,43 @@ class MainActivity : AppCompatActivity() {
 
 
 //            Kehadiran Dropdown=======================================
+            val kehadiranList = listOf (
+                "tepat waktu",
+                "terlambat",
+                "izin"
+            );
             val adapterKehadiran = ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_spinner_item,
-                kehadiranList
-            )
+                this@MainActivity,
+                android.R.layout.simple_spinner_item,kehadiranList)
             kehadiranSpinner.adapter = adapterKehadiran
 
 //            Selected Kehadiran
             kehadiranSpinner.onItemSelectedListener =
                 object : AdapterView.OnItemSelectedListener {
                     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+                        when (kehadiranList[position]) {
+                            "tepat waktu" -> {
+                                keteranganEdittext.visibility = View.GONE
+                            }
+                            "terlambat", "izin" -> {
+                                keteranganEdittext.visibility = View.VISIBLE
+                            }
+                        }
                     }
 
                     override fun onNothingSelected(parent: AdapterView<*>?) {
 
                     }
                 }
-
-
-
+            // Button Submit
+            submitButton.setOnClickListener {
+                // Display a toast message with the selected date and time
+                Toast.makeText(
+                    this@MainActivity,
+                    "Presensi berhasil $selectedDate jam $selectedTime",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
